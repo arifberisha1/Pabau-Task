ba107e52fdb4f6df107309d4c0090259a867f1dc
Arif2002 <arifberisha2002@gmail.com>
2023-06-10 00:28:57 +0200

    Getting the needed elements in index.js

---
+++ monorepo/JavaScript/index.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+
+const cbxInternship = document.getElementById("Internship");
+const cbxPabau = document.getElementById("Pabau");
+const cbxEmployment = document.getElementById("Employment");
+const cbxCareer = document.getElementById("Career");
+
+const btnShuffle = document.getElementById("shuffleBtn");
+const btnChange = document.getElementById("changeBtn");
+const btnShow = document.getElementById("showBtn");
+const btnReload = document.getElementById("reloadBtn");
+
+const display = document.getElementById("display");
+
+btnShuffle.onclick = shuffleValues;
+btnChange.onclick = changeValues;
+btnShow.onclick = printValues;
+btnReload.onclick = () => window.location.reload();
+
+function shuffleValues(){
+
+}
+
+function changeValues(){
+
+}
+
+function printValues(){
+
+}
